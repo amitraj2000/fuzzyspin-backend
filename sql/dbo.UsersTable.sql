beb02053c9a251a8a97c379cd98aa7d046639a07
Niraj Kumar Chauhan <mail.nirajchauhan>
2020-04-07 18:38:59 +0530

    Initial commit

---
+++ sql/dbo.UsersTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(150) NULL, 
+    [LastName] NVARCHAR(150) NULL, 
+    [ProfilePicture] NVARCHAR(200) NULL,
+	[PasswordHash]   NVARCHAR (150)     NULL,
+	[Salt]      NVARCHAR (MAX)     NULL,
+    [CreatedOn] DATETIMEOFFSET (7) NULL,
+	[UpdatedOn] DATETIMEOFFSET (7) NULL
+)
